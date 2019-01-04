250198985d7b8a40841d85d74243ae3883987f9b
Christopher Etges <c.etges86@gmail.com>
2019-01-04 16:20:27 -0700

    progress on pull requests

--- app.js
+++ app.js
@@ -7,20 +7,56 @@
 
 const inquirer = require("inquirer");
 const gh = require("parse-github-url");
+const https = require('https');
 
 
 inquirer.prompt([
     {
-        type:"input",
-        message:"What url would you like to view?",
-        name:"repoURL"
+        type: "input",
+        message: "What url would you like to view?",
+        name: "repoURL"
     }
 ])
-.then(function(res) {
-    console.log(res.repoURL);
-    let newURL = gh(res.repoURL);
-    console.log(newURL);
-    //API request
-    //sample reqURL : https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+    .then(function (res) {
+        let ghURL = gh(res.repoURL);
+        let username = ghURL.path;
 
-})
+        let options = {
+            host: 'api.github.com',
+            path: `/repos/${username}/pulls`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        };
+        //API request
+        //sample reqURL : https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+        //GET pull requests
+        let request = https.request(options, function (response) {
+            let body = [];
+            response.on("data", function (chunk) {
+                body += chunk.toString('utf8');
+            });
+
+            response.on("end", function () {
+                let pullResp = JSON.parse(body);
+
+                pullResp.forEach(pull => {
+                    console.log("-----------------------------")
+                    console.log(`Title: ${pull.title}`);
+                    console.log(`Number: ${pull.number}`);
+                    console.log(`Author: ${pull.user.login}`);
+                    console.log(`Body: ${pull.body}`);
+                    console.log("-----------------------------");
+
+
+
+                })
+                //console.log("Body: ", JSON.parse(body));
+                //GET /repos/:owner/:repo/pulls/:number/commits
+            });
+
+        });
+
+        request.end();
+
+
+    })
