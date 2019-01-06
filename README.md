adae63475939ca31bc36fec358b526bcba99240c
Christopher Etges <c.etges86@gmail.com>
2019-01-05 22:15:01 -0700

    final with comments

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # node-test
+
+Using the GitHub API ( https://developer.github.com/v3 ), we ask that you build
+a Node.js app that displays info for the open pull requests in a GitHub repository. The
+interface can take whatever form you like and should accept a repository URL, for
+example https://github.com/hapijs/hapi. It should display a list of open pull requests
+along with the number of commits in that PR, the number of comments on the PR, and
+the user that opened it.
--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+//Chris Etges
+
 const inquirer = require('inquirer');
 const gh = require('parse-github-url');
 const rp = require('request-promise');
 
-async function getPulls(username) {
+async function getPulls(urlPath) {
     try {
+        //initial API call for repo information, using request promises
         const res = await rp({
-            url: `https://api.github.com/repos/${username}/pulls`,
+            url: `https://api.github.com/repos/${urlPath}/pulls`,
             method: 'GET',
             headers: { 'User-Agent': 'Request-Promise' },
             json: true
         });
 
+        console.log(`${res.length} Open Pull Requests`);
+
+        //looping through the results JSON for commit and comment calls
         for (i = 0; i < res.length; i++) {
 
+            //async promises, length to determine number of commits/comments
             const commitCount = (await rp({
                 url: res[i].commits_url,
                 method: 'GET',
@@ -40,15 +47,12 @@ async function getPulls(username) {
     };
 };
 
+//CLI prompt
 inquirer.prompt([
     {
         type: 'input',
         message: 'What url would you like to view?',
         name: 'repoURL'
     }
-]).then(function (res) {
-    let ghURL = gh(res.repoURL);
-    let username = ghURL.path;
-
-    getPulls(username);
-});
+    //gh package parses inputted github URL for username and repo name, then calls async function 
+]).then(function (res) { getPulls(gh(res.repoURL).path) });
